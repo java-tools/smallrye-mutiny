2b9a78216f064ee8ec99a555224526fa59e2492d
Clement Escoffier <clement.escoffier@gmail.com>
2020-12-18 15:03:24 +0100

    Initial version of the cut-release bash script

---
+++ .build/cut-release.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -e
+
+init_gpg() {
+    gpg2 --fast-import --no-tty --batch --yes smallrye-sign.asc
+}
+
+init_git() {
+    git config --global user.name "${GITHUB_ACTOR}"
+    git config --global user.email "smallrye@googlegroups.com"
+
+    git update-index --assume-unchanged .build/deploy.sh
+    git update-index --assume-unchanged .build/decrypt-secrets.sh
+}
+
+# -------- SCRIPT START HERE -----------------
+
+init_git
+init_gpg
+
+export RELEASE_VERSION=""
+export BRANCH="HEAD"
+export EXTRA_PRE_ARGS=""
+
+if [[ ${MICRO_RELEASE} == "true" ]]; then
+  EXTRA_PRE_ARGS="--micro"
+fi
+
+if [[ ! -z "${USER_NAME}" ]]; then
+  EXTRA_PRE_ARGS="${EXTRA_PRE_ARGS} --username=${USER_NAME}"
+fi
+
+if [[ ! -z "${RELEASE_VERSION}" ]]; then
+  EXTRA_PRE_ARGS="${EXTRA_PRE_ARGS} --release-version=${RELEASE_VERSION}"
+fi
+
+jbang .build/PreRelease.java --token="${GITHUB_TOKEN}" "${EXTRA_PRE_ARGS}"
+
+export RELEASE_VERSION=""
+if [ -f /tmp/release-version ]; then
+  RELEASE_VERSION=$(cat /tmp/release-version)
+else
+    echo "'/tmp/release-version' expected after pre-release"
+    exit 1
+fi
+
+echo "Cutting release ${RELEASE_VERSION}"
+mvn -B -fn clean
+git checkout ${BRANCH}
+HASH=$(git rev-parse --verify $BRANCH)
+echo "Last commit is ${HASH} - creating detached branch"
+git checkout -b "r${RELEASE_VERSION}" "${HASH}"
+
+echo "Update version to ${RELEASE_VERSION}"
+mvn -B versions:set -DnewVersion="${RELEASE_VERSION}" -DgenerateBackupPoms=false -s maven-settings.xml
+mvn -B clean verify -Prelease -s maven-settings.xml
+
+echo "Update website version to ${RELEASE_VERSION}"
+sed -ie "s/mutiny_version: .*/mutiny_version: ${RELEASE_VERSION}/g" documentation/src/main/jekyll/_data/versions.yml
+
+git commit -am "[RELEASE] - Bump version to ${RELEASE_VERSION}"
+git tag "${RELEASE_VERSION}"
+echo "Pushing tag to origin"
+# git push origin "${RELEASE_VERSION}"
